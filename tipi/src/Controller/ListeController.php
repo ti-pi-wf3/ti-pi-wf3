b1704bc565603d4177db14714afee7883e0b59dd
Michael SARRAZIN <michael.sarrazin.ms@gmail.com>
2021-07-26 23:39:45 +0200

    nouveau départ avec correction des Form et entity

---
+++ tipi/src/Controller/ListeController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class ListeController extends AbstractController
+{
+    /**
+     * @Route("/liste", name="liste")
+     */
+    public function index(): Response
+    {
+        return $this->render('liste/index.html.twig', [
+            'controller_name' => 'ListeController',
+        ]);
+    }
+}
