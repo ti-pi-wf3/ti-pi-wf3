db9f2a0395434db942a9bed4d15f2037749d380b
Jennifer VINCHON-SIX <sixjennifer@ymail.com>
2021-07-26 21:47:26 +0200

    dev_jennifer_2307

--- tipi/src/Controller/CouncilController.php
+++ tipi/src/Controller/CouncilController.php
@@ -43,15 +43,21 @@ class CouncilController extends AbstractController
     /**
      * Créer 1 réunion : formulaire
      * 
-     * @Route("/addCouncil", name="addCouncil")
+     * @Route("/council/addCouncil", name="addCouncil")
+     * @Route("/council/{id}/edit", name="councilEdit")
      */
-    public function Council(Request $request, EntityManagerInterface $manager): Response
+    public function Council(Council $addCouncil = null, Request $request, EntityManagerInterface $manager): Response
     {
-        $addCouncil = new Council();
+        if(!$addCouncil) // Si la reunion n'existe pas (donc $addCouncil = null)
+        {
+            $addCouncil = new Council();
+        }
+        
         $formAddCouncil = $this->createForm(CouncilType::class, $addCouncil);
         $formAddCouncil->handleRequest($request); // recuperer les données de POST (du formulaire) et de les envoyé dans le bon objet
 
         dump($request); // permet de stocker toute les informations des superglobales ($_POST,$_GET, $_FILES...)
+        dump($addCouncil);
 
         // dump($userRepository);
 
@@ -59,7 +65,7 @@ class CouncilController extends AbstractController
 
         if($formAddCouncil->isSubmitted() && $formAddCouncil->isValid())
         {
-
+            // recupération de la clé étrangère:
             $user = $this->getUser();
 
             $addCouncil->setUser($user);
@@ -70,12 +76,46 @@ class CouncilController extends AbstractController
 
             $manager->flush();
 
-            return $this->redirectToRoute('council');
+            // *********** REDIRECTION sur 1 autre page à la validation du FORMULAIRE
+            return $this->redirectToRoute('councilShow', [
+                "id" => $addCouncil->getId()
+            ]);
         }
-
+        // *********** GENERER / AFFICHAGE du FORMULAIRE ds le template
         return $this->render('council/addCouncil.html.twig', [
             'formAddCouncil' => $formAddCouncil->createView(),
+            "editCouncil" => $addCouncil->getID() // on transmet l'ID de la réunion au template
     
         ]); 
+    }
+
+    /**
+     * Méthode permettant d'afficher le détail/ le contenu d'une REUNION
+     * 
+     * @Route("/council/{id}", name="councilShow")
+     */
+    public function show(Council $addCouncil, Request $request, EntityManagerInterface $manager): Response
+    {
+        // L'id transmit dans l'URL est envoyé directement en argument de la fonction show(), ce qui nous permet d'avoir accès à l'id de l'article a selectionner en BDD au sein de la méthode show()
+        // dump($id); // 6
+
+        // Importation de la classe CouncilRepository
+        // $repoCouncil = $this->getDoctrine()->getRepository(Council::class);
+        // dump($repoCouncil);
+
+        // find() : méthode mise à dispostion par Symfony issue de la classe CouncilRepository permettant de selectionner un élément de la BDD par son ID 
+        // $council : tableau ARRAY contenant toutes les données de la réunion selectionné en BDD en fonction de l'ID transmit dans l'URL 
+
+        // SELECT * FROM Council WHERE id = 6 + FETCH 
+        // $council = $repoCouncil->find($id); // 6
+        dump($request->server->get('DOCUMENT_ROOT'));
+
+        $this->addFlash("success", "Félicitations, Vous avez créer une réunion !");
+
+        // ****** ENVOIS les infos au TEMPLATE
+        // render() : méthode qui permet d'envoyer les info receptionner au dessu
+        return $this->render('council/showCouncil.html.twig', [
+            "councilBDD" => $addCouncil, // on transmet au template les données de la réunion selectionné en BDD afin de les traiter avec le langage TWIG ds le template
+        ]);
     }
 }
