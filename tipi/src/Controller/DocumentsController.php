a6b0e00efc459062d0003eef86f21d54bc951969
Kemering.Steven <kemering.steven@gmail.com>
2021-07-28 13:50:47 +0200

    CATEGORY ADD/EDIT/DELETE FINISH

--- tipi/src/Controller/DocumentsController.php
+++ tipi/src/Controller/DocumentsController.php
@@ -27,14 +27,18 @@ class DocumentsController extends AbstractController
         ]);
     }
 
+    // Ajout et Modification d'une catégorie pour les documents
     /**
-     *
-     * @Route("/categoryDocumentAdd", name="CategoryDocumentAdd")
-     * * AJOUTER UNE CATEGORIE pour les DOCUMENTS
+     * @Route("/categoryDocumentAdd", name="categoryDocumentAdd")
+     * @Route("/categoryDocumentEdit/{id}", name="categoryDocumentEdit")
      */
-    public function CategoryDocumentAdd(Request $request, EntityManagerInterface $manager): Response
+    public function CategoryDocumentAdd(CategoryDocument $CategoryDocumentNew = null, Request $request, EntityManagerInterface $manager): Response
     {
-        $CategoryDocumentNew = new CategoryDocument();
+        if(!$CategoryDocumentNew)
+        {
+            $CategoryDocumentNew = new CategoryDocument();
+        }
+
         $formCategoryDocumentAdd = $this->createForm(CategoryDocumentAddType::class, $CategoryDocumentNew);
         $formCategoryDocumentAdd->handleRequest($request);
 
@@ -42,17 +46,32 @@ class DocumentsController extends AbstractController
         {
 
             $manager->persist($CategoryDocumentNew);
-
             $manager->flush();
 
-            return $this->redirectToRoute('CategoryDocumentAdd');
+            $this->addFlash('success', 'Le nom de la catégorie a bien été enregistrés !');
+
+            return $this->redirectToRoute('viewCategoryDocument');
         }
 
+        // return $this->render('documents/categoryDocumentAdd.html.twig', [
         return $this->render('documents/categoryDocumentAdd.html.twig', [
             'controller_name' => 'Ajouter une catégorie de document',
             'formCategoryDocumentAdd' => $formCategoryDocumentAdd->createView(),
+            'editMode' => $CategoryDocumentNew->getId() !== null
         ]);
     }
+
+    // Suppression d'une catégorie pour les documents
+    /**
+     * @Route("/categoryDocumentDelete/{id}", name="categoryDocumentDelete")
+     */
+    public function categoryDocumentDelete(CategoryDocument $CategoryDocumentDelete, EntityManagerInterface $manager): \Symfony\Component\HttpFoundation\RedirectResponse
+    {
+         $manager->remove($CategoryDocumentDelete);
+         $manager->flush();
+         $this->addFlash('success', "La catégorie a bien été supprimée !");
+         return $this->redirectToRoute('viewCategoryDocument');
+     }
 
     /**
      * @Route("/viewCategoryDocument", name="viewCategoryDocument")
@@ -74,7 +93,7 @@ class DocumentsController extends AbstractController
     }
 
         /**
-     * @Route("/documentAdd", name="DocumentAdd")
+     * @Route("/documentAdd", name="documentAdd")
      * * AJOUTER UN DOCUMENT
      */
     public function DocumentAdd(Request $request, EntityManagerInterface $manager): Response
@@ -97,7 +116,7 @@ class DocumentsController extends AbstractController
             $manager->persist($DocumentNew);
             $manager->flush();
 
-            return $this->redirectToRoute('DocumentAdd');
+            return $this->redirectToRoute('documentAdd');
         }
 
         return $this->render('documents/documentAdd.html.twig', [
