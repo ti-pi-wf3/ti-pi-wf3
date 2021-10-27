bd0818642dbc9eabdbeec78aa4bea5e100c95cba
Steven Kemering <kemering.steven@gmail.com>
2021-10-27 09:24:55 +0200

    config .env deploy

--- deploy.php
+++ deploy.php
@@ -1,16 +1,19 @@
 <?php
 namespace Deployer;
 
-require 'recipe/symfony.php';
+require 'recipe/symfony4.php';
 
 // Project name
-set('application', 'my_project');
+set('application', 'tipi');
 
 // Project repository
 set('repository', 'https://github.com/ti-pi-wf3/ti-pi-wf3.git');
 
 // [Optional] Allocate tty for git clone. Default value is false.
-set('git_tty', true); 
+set('git_tty', false);
+
+set('ssh_multiplexing', false);
+set('composer_options', '{{composer_action}} --prefer-dist --no-progress --no-interaction --no-dev --optimize-autoloader');
 
 // Shared files/dirs between deploys 
 add('shared_files', []);
@@ -18,23 +21,88 @@ add('shared_dirs', []);
 
 // Writable dirs by web server 
 add('writable_dirs', []);
+set('allow_anonymous_stats', false);
 
 
 // Hosts
-
-host((array)'109.106.246.101')
-    ->set('deploy_path', '~/{{application}}');    
+host('localhost')
+    ->set('deploy_path', '~/{{application}}')
+    ->set('http_user', 'klawtek.fr')
+;
     
 // Tasks
+
+// Tasks
+task('symlink:public', function() {
+    run('ln -s {{release_path}}/public/*  /www &&  ln -s {{release_path}}/public/.[^.]* /www');
+});
+
+task('cache:clear', function () {
+    run('php {{release_path}}/bin/console cache:clear');
+});
+
+/* Is used when symlink from public folder doesn't behave as expected.
+ * The downside of using it this way is that it doesn't remove files no longer present in git repo.
+ * Assumed public directory is /www
+ */
+task('copy:public', function() {
+    run('cp -R {{release_path}}/public/*  /www && cp -R {{release_path}}/public/.[^.]* /www');
+});
+
+/* Uploads built assets from local to remote. Requires rsync.
+ * Useful when you use Symfony encore/webpack and remote machine doesn't support npm/yarn.
+ */
+task('upload:build', function() {
+    upload("public/build/", '{{release_path}}/public/build/');
+});
+
+task('upload:build', function() {
+    upload("public/build/", '{{release_path}}/public/build/');
+});
+
+task('init:database', function() {
+    run('{{bin/php}} {{bin/console}} doctrine:schema:create');
+});
+
+task('echo:options', function() {
+    writeln('OPTIONS: {{composer_options}}');
+});
 
 task('build', function () {
     run('cd {{release_path}} && build');
 });
 
+task('initialize', [
+    'deploy:info',
+    'deploy:prepare',
+    'deploy:lock',
+    'deploy:release',
+    'deploy:update_code',
+    'deploy:shared',
+    'deploy:unlock',
+    'cleanup',
+]);
+
+task('mydeploy', [
+    'deploy:info',
+    'deploy:prepare',
+    'deploy:lock',
+    'deploy:release',
+    'deploy:update_code',
+    'deploy:shared',
+    'deploy:vendors',
+    'deploy:cache:clear',
+    'deploy:cache:warmup',
+    'deploy:symlink',
+    'copy:public',
+    'deploy:unlock',
+    'cleanup',
+]);
+
 // [Optional] if deploy fails automatically unlock.
 after('deploy:failed', 'deploy:unlock');
+//after('deploy:unlock', 'copy:public');
+
 
 // Migrate database before symlink new release.
-
 before('deploy:symlink', 'database:migrate');
-
